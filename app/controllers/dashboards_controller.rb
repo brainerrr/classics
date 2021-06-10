c6a775550a63854114f9eab86df375b25d811b5c
Bogdan Dan <dan.f.bogdan@gmail.com>
2021-06-10 13:02:38 +0200

    booking request

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,6 @@ class DashboardsController < ApplicationController
   def account
     @my_bookings = Booking.where(user: current_user)
     # @my_cars_bookings = User.id.where(current_user.cars.bookings)
+    @all_bookings = Booking.all
   end
 end
