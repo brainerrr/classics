1cf7059e99270e36eb41d33293510193492593b7
Georg Liechtenstein <liechtensteingeorg@gmail.com>
2021-06-10 12:16:26 +0200

    dashboard first steps one

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'account', to: 'dashboards#account'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cars, only: [:new, :index, :create, :show] do
      resources :bookings, only: [:new, :create, :index, :show]
