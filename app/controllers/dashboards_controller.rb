ac7fd5c11502656a604974300042a980db6b59c1
Georg Liechtenstein <liechtensteingeorg@gmail.com>
2021-06-10 18:25:05 +0200

    Fix

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def account
+    @review = Review.new
     @my_bookings = Booking.where(user: current_user)
     # @my_cars_bookings = User.id.where(current_user.cars.bookings)
     @all_bookings = Booking.all
